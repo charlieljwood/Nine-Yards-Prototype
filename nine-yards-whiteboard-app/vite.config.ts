3ee5dc97dbec0c1593713fa14f4ad34e4d0dd06b
Charlie Wood <23cw3414@barton.ac.uk>
2024-06-26 21:09:44 +0100

    Updated workflows and vite config

--- nine-yards-whiteboard-app/vite.config.ts
+++ nine-yards-whiteboard-app/vite.config.ts
@@ -3,6 +3,7 @@ import solidPlugin from 'vite-plugin-solid';
 // import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
+  base: "/Nine-Yards-Prototype/",
   plugins: [
     /* 
     Uncomment the following line to enable solid-devtools.
